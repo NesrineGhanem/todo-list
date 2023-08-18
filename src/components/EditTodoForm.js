90e747fdb902138a8ca4fc216f0675ab3d016fde
5h140w <5h140w23@gmail.com>
2023-08-18 19:53:05 +0100

    changess

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 
-export const EditTodoForm = ({editTodo , task}) => {
-    const [value, setValue] = useState(task.task)
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
 
-    const handleSubmit = (e) => {
-       e.preventDefault();
-       editTodo(value , task.id)
-       setValue("")
-    }
-    return (
-        <>
-        
-        <form className="TodoForm" onSubmit={handleSubmit}>
-        <input type="text" className="todo-input" 
-        placeholder="update task "/>
-        <button type="submit" className="todo-btn" 
-        value={value}
-        onChange={(e)=> setValue(e.target.value)}
-        >update Task
-            </button>
-        </form>
-        </>
-    )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editTodo(value, task.id);
+    setValue("");
+  };
+  return (
+    <>
+      <form className="TodoForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="update task "
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit" className="todo-btn">
+          update Task
+        </button>
+      </form>
+    </>
+  );
+};
